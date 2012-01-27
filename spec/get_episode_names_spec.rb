21e352293567f297565b9fafa94826fa14593090
Mark <sirkenix@gmail.com>
2012-01-27 21:27:58 +0100

    Refactoring
    Exported the GetEpisodeNames class into a seperate file in the lib
    directory.

--- spec/get_episode_names_spec.rb
+++ spec/get_episode_names_spec.rb
@@ -3,6 +3,8 @@ require_relative 'spec_helper'
 require 'minitest/spec'
 require 'minitest/autorun'
 
+require_relative '../lib/get_episode_names'
+
 describe "GetEpisodeNames" do
 
   it "takes the name of the series" do
@@ -18,7 +20,6 @@ describe "GetEpisodeNames" do
       series_name = "Burn Notice"
       SeriesNamer::GetEpisodeNames.new(series_name).series_info.name.must_equal series_name
     end
-
   end
 
   it "throws an exception if the series does not exist" do
@@ -50,56 +51,3 @@ describe "GetEpisodeNames" do
     end
   end
 end
-
-module SeriesNamer
-
-  require 'tvdb_party'
-
-  class GetEpisodeNames
-
-    attr_reader :series_name, :series_info
-
-    def initialize( series_name, tvdb_client = TvdbParty::Search.new("E923104E8EF02C8F") )
-      @series_name = series_name
-      @series_info = setup_client(tvdb_client)
-    end
-
-    def episode_name( season, episode )
-      begin
-        episode_info = @series_info.get_episode(season, episode)
-      rescue VCR::Errors::UnhandledHTTPRequestError, SocketError
-        episode_info = nil
-      end
-
-
-      raise ArgumentError, "Episode #{episode} Season #{season} not found." if episode_info.nil?
-
-      episode_name = "S" + "%02d" % season + "E" + "%02d" % episode + " "
-      episode_name << episode_info.name
-
-      return episode_name
-    end
-
-    def episode_names( season )
-      raise NotImplementedError
-    end
-
-    private
-
-    def setup_client( tvdb_client )
-      begin
-        results = tvdb_client.search(@series_name).first
-      rescue VCR::Errors::UnhandledHTTPRequestError, SocketError
-        results = nil
-      end
-
-      raise ArgumentError, @series_name.to_s + " was not found" if results.nil?
-
-      series = tvdb_client.get_series_by_id(results["seriesid"])
-
-      return series
-    end
-
-  end
-end
-
