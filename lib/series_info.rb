f841f7180e4d513bc36a219f809d23ff50c398fc
Mark <sirkenix@gmail.com>
2012-01-28 15:15:46 +0100

    Added support for episodes in the season info file
    Added a further spec for the series info module.

--- lib/series_info.rb
+++ lib/series_info.rb
@@ -6,9 +6,34 @@ module SeriesNamer
       @path = path
       @series = series
 
-      @seasons = []
-      @seasons << seasons
-      @seasons.flatten!
+      @seasons = Hash.new
+      if( seasons.kind_of?( Array ) )
+        seasons.each{ |season| @seasons[season] = Array.new }
+      else
+        @seasons[seasons] = Array.new
+      end
+    end
+
+    # Get all seasons
+    def seasons
+      return @seasons.keys
+    end
+
+    # Get all episodes for a season
+    def episodes( season )
+      return @seasons[season]
+    end
+
+    # add multiple episdoes to a season
+    def add_episodes( season, episodes)
+      @seasons[season].concat(episodes)
+      @seasons[season].flatten
+    end
+
+    # add single episode to season
+    def add_episode( season, episode)
+      @seasons[season] << episode
+      @seasons[season].flatten
     end
   end
 end
---
+++ spec/series_info_spec.rb
@@ -0,0 +1,54 @@
+require 'minitest/spec'
+require 'minitest/autorun'
+
+require_relative 'spec_helper'
+require_relative '../lib/series_info'
+
+describe "SeriesInfo" do
+    def setup
+      @path = "gnabber/schnabber"
+      @series = "Burn Notice"
+      @seasons = ["season 1","season 2"]
+    end
+  describe "When initializing it" do
+    it "must store path, series and seasons" do
+
+      series_info = SeriesNamer::SeriesInfo.new( @path, @series, @seasons )
+
+      series_info.path.must_equal @path
+      series_info.series.must_equal @series
+      series_info.seasons.must_equal @seasons
+    end
+
+    it "is possible to pass in only one season" do
+      seasons = "season 1"
+
+      series_info = SeriesNamer::SeriesInfo.new( @path, @series, seasons )
+
+      series_info.path.must_equal @path
+      series_info.series.must_equal @series
+      series_info.seasons.must_equal [seasons]
+    end
+  end
+
+  describe "Adding further content" do
+    it "is possible to add episodes" do
+      episodes = ["episode1", "episode2"]
+
+      series_info = SeriesNamer::SeriesInfo.new( @path, @series, @seasons )
+      series_info.add_episodes(@seasons.first, episodes)
+
+      series_info.episodes( @seasons.first ).must_equal episodes
+    end
+
+    it "is possible to add only one episode to a season" do
+      episode = "episode1"
+
+      series_info = SeriesNamer::SeriesInfo.new( @path, @series, @seasons )
+      series_info.add_episode(@seasons.first, episode)
+
+      series_info.episodes( @seasons.first ).must_equal [episode]
+    end
+  end
+end
+
