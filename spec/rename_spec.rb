4bc14b24d26cb65c8d54fbcfdf2216077e7b7b86
Mark <sirkenix@gmail.com>
2012-02-02 08:57:28 +0100

    Added additional checks for mocks, refacotring
    Added checks if the mocks are called as expected. Further refactored the
    class under tests i.e. Rename so that the entry (public method) now is
    more readable by extracting large method calls into private methods.

--- spec/rename_spec.rb
+++ spec/rename_spec.rb
@@ -1,13 +1,16 @@
+require_relative 'spec_helper'
+
 require 'minitest/spec'
 require 'minitest/autorun'
 
-require_relative 'spec_helper'
+require_relative '../lib/series_info'
 
 describe "The manager object" do
   describe "executes the given objects in the right order" do
 
     def setup
       @path = "Burn Notice/Season 1"
+      #@path = SeriesNamer::SeriesInfo.new("Burn Notice/Season 1","Burn Notice","Season 1")
     end
 
     it "takes a path" do
@@ -27,26 +30,65 @@ describe "The manager object" do
 
       dir_mock = MiniTest::Mock.new
       dir_mock.expect(:exists?, true, [@path] )
-      dir_mock.expect(:entries, Array.new(given_entries), [@path])
+      dir_mock.expect(:entries, given_entries, [@path])
 
-      SeriesNamer::Renamer.new( @path, dir_mock, DummyFile.new ).entries.must_equal expected_entries
+      VCR.use_cassette('burn_notice_season1_all_episodes') do
+        renamer = SeriesNamer::Renamer.new( @path, dir_mock, DummyFile.new )
+        renamer.execute
+        renamer.series_info.episodes("Season 1").must_equal expected_entries
+      end
+
+      dir_mock.verify.must_equal true
     end
 
     it "it gets all the episode names per season" do
-      skip
+      expected_entries = ["S01E01 Burn Notice", "S01E02 Identity", "S01E03 Fight or Flight"]
+      given_entries = ["episode 1", "episode 2", "episode 3", ".", "..", "dir_something"]
+
+      dir_mock = MiniTest::Mock.new
+      dir_mock.expect(:exists?, true, [@path] )
+      dir_mock.expect(:entries, given_entries, [@path])
+
+      VCR.use_cassette('burn_notice_season1_all_episodes') do
+        renamer = SeriesNamer::Renamer.new( @path, dir_mock, DummyFile.new )
+        renamer.execute
+        renamer.series_info.new_episodes(renamer.series_info.seasons.first).must_equal expected_entries
+      end
+
+      dir_mock.verify.must_equal true
     end
 
-    it "renames all the episodes per season"
+    it "renames all the episodes per season" do
+      skip
+      expected_entries = ["S01E01 Burn Notice", "S01E02 Identity", "S01E03 Fight or Flight"]
+      given_entries = ["episode 1", "episode 2", "episode 3", ".", "..", "dir_something"]
+
+      dir_mock = MiniTest::Mock.new
+      dir_mock.expect(:exists?, true, [@path] )
+      dir_mock.expect(:entries, given_entries, [@path])
+
+      VCR.use_cassette('burn_notice_season1_all_episodes') do
+        renamer = SeriesNamer::Renamer.new( @path, dir_mock, DummyFile.new )
+        renamer.execute
+        renamer.series_info.
+          new_episodes(renamer.series_info.seasons.first).
+          must_equal expected_entries
+      end
+
+      dir_mock.verify.must_equal true
+    end
   end
 end
 
 module SeriesNamer
 
   require_relative '../lib/find_episodes'
+  require_relative '../lib/get_episode_names'
+  require_relative '../lib/parse_path'
 
   class Renamer
 
-    attr_reader :path
+    attr_reader :path, :series_info
 
     @dir #defaults to ruby dir used for tests
     @file #defaults to ruby file used for tests
@@ -60,8 +102,27 @@ module SeriesNamer
       @file = file
     end
 
-    def entries
-      return FindEpisodes.new(@path, @dir, @file).results
+    def execute
+      @series_info = ParsePath.new(@path, @dir, @file).series_info
+
+      @series_info.add_episodes(@series_info.seasons.first, get_dir_entries)
+
+      @series_info.add_new_episodes(
+        @series_info.seasons.first, get_episode_names
+      )
+
+    end
+
+    private
+
+    def get_dir_entries
+      return FindEpisodes.new(@series_info, @dir, @file).results
+    end
+
+    def get_episode_names
+      return GetEpisodeNames.new(@series_info).episode_names(
+        @series_info.seasons.first[@series_info.seasons.first.size-1].to_i, 
+        @series_info.episodes(@series_info.seasons.first).size )
     end
   end
 
